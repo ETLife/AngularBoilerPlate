7fb6ed534955afa7b1421611248f655c56456419
Randy1Burrell <Randy1burrell@yahoo.com>
2017-12-02 20:32:09 -0500

    Requiring different file paths

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 require('angular');
 require('angular-route');
 require('angular-sanitize');
+require('./app/module/module.js');
 require('./app/app.js');
+require('./app/home/home.controller.js');
+
